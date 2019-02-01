fc136c85361b46b30164a908e3973f19964a73a1
Irdi Caushi <irdicaushi@hotmail.com>
2019-02-01 17:18:56 +0200

    Last Program

--- python.py
+++ python.py
@@ -6,3 +6,15 @@ def my_function(var1, var2):
 
 print(autoReturn())
 print(my_function(4,5))
+
+
+my_list=[50,24,26]
+my_list.sort()
+
+print(my_list)
+print(sum(my_list))
+
+
+my_list.append(2)
+
+my_tuple = (7,)
