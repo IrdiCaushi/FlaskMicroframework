4b4423fb8d90743c13715d5b65853281841f7b1c
Irdi Caushi <irdicaushi@hotmail.com>
2019-02-08 14:55:32 +0200

    Class exercise, 2nd week

---
+++ conditionals.py
@@ -0,0 +1,39 @@
+# i_am_true = True
+# i_am_false = False
+# i_am_test = "I am really a text"
+
+# if i_am_true is True:
+#     print("I am really a text")
+
+# if i_am_false is True:
+#     print("I am really a text")
+# else:
+#     print("I am else")
+
+# if "really" in i_am_test:
+#     print("{} string found!".format("really"))
+
+# Practical Exercise Start
+
+def even(number):
+    if (number%2 == 0):
+        return True
+
+user_input=int(input("Please enter the number!\n"))
+
+if even(user_input)==True:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+
+name_list = ["John","Jack","Jim","Johhny"]
+
+def name_function(username):
+    if username in name_list:
+        print(username)
+
+name_function("Irdi")
+name_function("John")
+
+# Practical Exercise End
